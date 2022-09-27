fc19e475346ed7755f46a3056accb2bceda3206a
JohnHofbauer <98779922+JohnHofbauer@users.noreply.github.com>
2022-09-26 21:20:04 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Java script, for the first div to create the parallax affect. 
-
-const parallax = document.getElementById("parallax1");
-window.addEventListener("scroll", function () {
-    let offset = window.pageYOffset;
-    //console.log('Offset: ' + offset);
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
-})
